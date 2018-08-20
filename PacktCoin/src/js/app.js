5de537c658d45d3b3584151622fe52973c603127
Richard Horrocks <richardjhorrocks@gmail.com>
2018-08-20 16:08:19 +0100

    Finished ICO code

--- PacktCoin/src/js/app.js
+++ PacktCoin/src/js/app.js
@@ -3,22 +3,19 @@ App = {
   contracts: {},
   account: '0x0',
   loading: false,
-  tokenPrice: 1000000000000000,
+  tokenPrice: 0,
   tokensSold: 0,
   tokensAvailable: 500000,
 
   init: function() {
-    console.log("App initialized...")
     return App.initWeb3();
   },
 
   initWeb3: function() {
     if (typeof web3 !== 'undefined') {
-      // If a web3 instance is already provided by Meta Mask.
       App.web3Provider = web3.currentProvider;
       web3 = new Web3(web3.currentProvider);
     } else {
-      // Specify default instance if no web3 instance provided
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
       web3 = new Web3(App.web3Provider);
     }
@@ -43,7 +40,7 @@ App = {
         App.listenForEvents();
         return App.render();
       });
-    })
+    });
   },
 
   // Listen for events emitted from the contract
@@ -55,8 +52,8 @@ App = {
       }).watch(function(error, event) {
         console.log("event triggered", event);
         App.render();
-      })
-    })
+      });
+    });
   },
 
   render: function() {
